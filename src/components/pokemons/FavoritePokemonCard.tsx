0f57d4725de8a8404d92951c864009439b0d6126
mufrick01 <mrmufrick@gmail.com>
2024-09-07 22:34:04 -0400

    favorite pokemon page added

--- src/components/pokemons/FavoritePokemonCard.tsx
+++ src/components/pokemons/FavoritePokemonCard.tsx
@@ -1,9 +1,10 @@
 import type { FavoritePokemon } from "@interfaces/favorite-pokemon"
-import { createSignal, type Component } from "solid-js"
+import { createSignal, Show, type Component } from "solid-js"
 
 interface Props {
     pokemon: FavoritePokemon
 }
+
 
 export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
 
@@ -11,12 +12,27 @@ export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
     const [isVisible, setIsVisible] = createSignal(true)
     const imgSource = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
 
+    const deletePokemonFromLocalStorage = () => {
+
+        const favorites = JSON.parse(localStorage.getItem('favorites') ?? '[]') as FavoritePokemon[]
+        const newFavorites = favorites.filter(p => p.name !== name)
+        localStorage.setItem('favorites', JSON.stringify(newFavorites))
+        setIsVisible(false);
+
+    }
+
+
     return (
-        <div class="flex flex-col justify-center items-center">
-            <a href={`/pokemons/${name}`}>
-                <img src={imgSource} alt="" />
-                <h2 class="text-center capitalize">#{id} {name}</h2>
-            </a>
-        </div>
+        <Show when={isVisible()}>
+            <div class="flex flex-col justify-center items-center">
+                <a href={`/pokemons/${name}`}>
+                    <img src={imgSource} alt="" style={`view-transition-name:${name}-image`} />
+                    <h2 class="text-center capitalize">#{id} {name}</h2>
+                </a>
+                <button class="text-red-700" onClick={deletePokemonFromLocalStorage}>
+                    Eliminar
+                </button>
+            </div>
+        </Show>
     )
 }
